baa9bcd5e444fc4c9155aad2f2f28f20714dd161
a <b>
2022-09-23 12:32:02 +0300

    added example with setter and logic

--- OOP/OOP/Tema 1 Demo/Program.cs
+++ OOP/OOP/Tema 1 Demo/Program.cs
@@ -21,15 +21,27 @@ namespace Tema_1_Demo
 
             //var test = new Purvi.Calculator();
 
-            var dice1 = new Dice();
-            dice1.Sides = 18;
-            
+            //var dice1 = new Dice();
+            //dice1.Sides = 18;
+
+            //while (true)
+            //{
+            //    var rollResult = dice1.Roll();
+            //    Console.WriteLine("Result from Roll: " + rollResult);
+            //    Thread.Sleep(5000);
+            //}
+
+            var person1 = new Person();
+            person1.Name = "Osama";
             while (true)
             {
-                var rollResult = dice1.Roll();
-                Console.WriteLine("Result from Roll: " + rollResult);
-                Thread.Sleep(5000);
+                Console.Write($"Kakva e vuzrastta na {person1.Name}:");
+                person1.Age=int.Parse(Console.ReadLine());
+                Console.WriteLine($"Vuzrastta na {person1.Name} e:{person1.Age}");
+                Console.WriteLine(new String('=',20));
             }
+
+
         }
     }
     class Dice
@@ -39,10 +51,10 @@ namespace Tema_1_Demo
 
         public int Sides
         {
-            get => sides;  set => sides = value;
+            get => sides; set => sides = value;
         }
 
-   
+
 
         public int Roll()
         {
@@ -51,4 +63,54 @@ namespace Tema_1_Demo
             return result;
         }
     }
+
+    public class Person
+    {
+        public string Name { get; set; }
+
+        private int age;
+
+        public int Age
+        {
+            get => age;
+
+            set
+            {
+
+                if (value < 0)
+                {
+                    Console.WriteLine("Nedei pisa gluposti!!!");
+                    age = 0;
+                    return;
+                }
+                if (value > 100)
+                {
+                    Console.WriteLine("Mnogo golqma vuzrast!!!");
+                    age = value;
+                    return;
+                }
+
+                if (value % 2 == 0)
+                {
+                    age = 2 * value;
+
+                }
+                else
+                {
+                    age = value;
+                }
+
+
+            }
+        }
+
+
+
+
+
+        //    public string Age { get=>age.ToString(); set=>age=int.Parse(value); }
+
+    }
+
+
 }
