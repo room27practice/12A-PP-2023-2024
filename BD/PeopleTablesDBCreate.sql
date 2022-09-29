e005298bfca49e60819856cce18beed3917cbf05
a <b>
2022-09-29 09:46:41 +0300

    ADDED INSERT AND DELETE AND SELECT STATEMENTS

--- BD/PeopleTablesDBCreate.sql
+++ BD/PeopleTablesDBCreate.sql
@@ -38,3 +38,30 @@ CONSTRAINT Age_range CHECK (Age BETWEEN 1 AND 125)
 ) 
 GO
 
+
+
+INSERT INTO Countries VALUES
+('Bulgaria',6.5,'BG','Europe','Bulgarian','Visoki sini planini, Reki I zlatni ravnini.'),
+('Uganda',45,'UG','Africa','English','For God and My Country.'),
+('USA',329,'US','South America','English, Spanish','In God We Trust.'),
+('Macedonia',2,'MK','Europe','Macedonian',null)
+
+SELECT * FROM Countries
+SELECT * FROM Towns
+
+INSERT INTO Towns VALUES
+('Shumen',89000,'9700',1, 1),
+('Plovdiv',343000,'4000', 1, 2),
+('Smiadovo',4044,'9820', 1, 1),
+('Richmond',229000,'V4K',3,null),
+('Kampala',1500000,'759125',2, 5)
+
+DELETE FROM Towns
+DBCC CHECKIDENT ('Towns', RESEED, 0)
+
+
+SELECT * FROM People
+INSERT INTO People(FirstName,SurnName,LastName,Age,Gender,Email,Id,TownId,Description)
+(
+
+
