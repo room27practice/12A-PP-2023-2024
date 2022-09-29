d639e771f6315ceb7bd553c1aeb4064808d9e5d0
a <b>
2022-09-29 11:20:27 +0300

    ADDED INSERT FOR PEOPLE AND SELECT STATEMENTS

--- BD/PeopleTablesDBCreate.sql
+++ BD/PeopleTablesDBCreate.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE HoraDurjaviGradove
+﻿CREATE DATABASE HoraDurjaviGradove
 GO
 USE HoraDurjaviGradove
 GO
@@ -24,7 +24,7 @@ FOREIGN KEY REFERENCES Towns(Id)
 )
 GO
 CREATE TABLE People(
-Id NVARCHAR PRIMARY KEY,
+Id NVARCHAR(64) PRIMARY KEY,
 [FirstName] VARCHAR(16) NOT NULL,
 [SurnName] VARCHAR(16),
 [LastName] VARCHAR(16) NOT NULL,
@@ -37,8 +37,6 @@ FOREIGN KEY REFERENCES Towns(Id),
 CONSTRAINT Age_range CHECK (Age BETWEEN 1 AND 125)
 ) 
 GO
-
-
 
 INSERT INTO Countries VALUES
 ('Bulgaria',6.5,'BG','Europe','Bulgarian','Visoki sini planini, Reki I zlatni ravnini.'),
@@ -59,9 +57,27 @@ INSERT INTO Towns VALUES
 DELETE FROM Towns
 DBCC CHECKIDENT ('Towns', RESEED, 0)
 
-
 SELECT * FROM People
-INSERT INTO People(FirstName,SurnName,LastName,Age,Gender,Email,Id,TownId,Description)
-(
+
+INSERT INTO People(FirstName,SurnName,LastName,Age,Gender,Email,TownId,[Description],Id) VALUES
+('Kosta','Ivanov','Mladenov',35,1,'kom@abv.bg',3,NULL,'f402e41e-78aa-4559-a495-4453b528a959'),
+('Malinka',null ,'Ivanova',25,0,'malimali@yahoo.com',2,	null,'a3d610da-9997-4e58-855a-e2af51d4048e'),
+('Kamelia','Amelieva' ,'Preslavova',21,0,'kamali@gmail.com',1,'GenshinImpact',	'066557c7-2333-4209-8982-0833d86a97c2'),
+('Marin',null , 'Goleminov',40,1,'marin_g@gmail.com',1,null,'4780bdf4-ba71-4657-be2d-b0dcb389bd08'),
+('Sambo','Viktor','Dontworry',58,1,'sambomambo@ugabuga.com', 5,null,'20648ab1-c5ea-4099-90ed-a77b5e173994'),
+('John', 'Bob', 'Marley', 17,1,'johnybegood@yahoo.com', 4,null,'d91d8c64-e627-4502-864b-6df244d7dea6'),
+('Jessica', 'Jehna', 'Jahmeson', 27,0,'jehna27@gmail.com', 4, null,'0154e4fb-bed2-40a6-bdf1-ab9ba00ec621')
+
+--/DROP TABLE People
+
+SELECT * FROM Countries
+SELECT * FROM Towns
+SELECT * FROM People
+
+SELECT [FirstName] as 'Първо име',[LastName],[Gender] as 'Пол',[Id] FROM People
+
+--SELECT TOP(2) [Name],[PostalCode] as Post FROM Towns AS T
 
 
+SELECT * FROM Countries
+TRUNCATE TABLE Countries
