c47ac5dbbdaa0253268325598bc27b5d6245ad97
a <b>
2022-11-08 11:39:11 +0200

    add

---
+++ OOP/GeographyDemoLINQ/GeographyMaster/Geo/Models/River.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Geo.Models
+{
+    public partial class River
+    {
+        public River()
+        {
+            CountriesRivers = new HashSet<CountriesRiver>();
+        }
+
+        public River(string riverName, int length, int drainageArea, int averageDischarge, string outflow)
+        {
+            RiverName = riverName;
+            Length = length;
+            DrainageArea = drainageArea;
+            AverageDischarge = averageDischarge;
+            Outflow = outflow;
+        }
+
+        public int Id { get; set; }
+        public string RiverName { get; set; }
+        public int Length { get; set; }
+        public int DrainageArea { get; set; }
+        public int AverageDischarge { get; set; }
+        public string Outflow { get; set; }
+
+        public virtual ICollection<CountriesRiver> CountriesRivers { get; set; }
+    }
+}
---
+++ OOP/GeographyDemoLINQ/GeographyMaster/Geo/Program.cs
@@ -0,0 +1,82 @@
+﻿using Geo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Geo
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            using (var db = new GeographyDBContext())
+            {
+                #region AddingNewRiver
+                //Country countryFd = db.Countries.FirstOrDefault(x => x.CountryCode == "BG");
+                //River newRiver = new River("Lom", 92500, 1240, 739, "Black Sea");
+
+                //var newRiverCountry = new CountriesRiver()
+                //{
+                //    River = newRiver
+                //};
+
+                //countryFd.CountriesRivers.Add(newRiverCountry);
+
+                ////countryFd.Rivers
+                ////    .Add(newRiver);
+                //db.SaveChanges();
+                #endregion
+
+                var riverNames = db.Rivers
+                    .Where(r => r.CountriesRivers.Any(c => c.CountryCode == "BG"))
+                    .Select(r => r.RiverName).Distinct()
+                    .ToArray();
+
+                List<River> approvedRivers = new List<River>();
+                foreach (var rName in riverNames)
+                {
+                    approvedRivers
+                        .Add(db.Rivers.FirstOrDefault(r => r.RiverName == rName));
+                }
+
+                var countryFound = db.Countries
+                    .Include(x => x.CountriesRivers)
+                    .ThenInclude(x => x.River)
+                    .First(x => x.CountryName == "Bulgaria");
+
+                countryFound.CountriesRivers = countryFound.CountriesRivers
+                    .Where(x => approvedRivers.Contains(x.River)).ToList();
+
+                db.SaveChanges();
+
+                //var countryFound = db.Countries
+                //    .Include(x => x.CountriesRivers)
+                //    .ThenInclude(x => x.River)
+                //    .First(x => x.CountryName == "China");
+
+                //var riverNames2 = countryFound.CountriesRivers
+                //    .OrderBy(r => r.River.Length)
+                //    .Select(x => $"{x.River.RiverName}: {x.River.Length}km")
+                //    .ToArray();
+
+
+               // Console.WriteLine(String.Join("\n", riverNames));
+                //Console.WriteLine(new String('=', 20));
+                //Console.WriteLine(String.Join("\n", riverNames2));
+
+
+
+
+
+
+            }
+
+
+
+
+
+        }
+    }
+}
