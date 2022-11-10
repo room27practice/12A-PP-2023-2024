29ef97376daf76bfbc1406f5d6acc60c2eef3570
a <b>
2022-11-10 11:40:21 +0200

    added demo of LINQ queries

--- OOP/GeographyDemoLINQ/GeographyMaster/Geo/Program.cs
+++ OOP/GeographyDemoLINQ/GeographyMaster/Geo/Program.cs
@@ -13,85 +13,144 @@ namespace Geo
             using (var db = new GeographyDBContext())
             {
                 #region AddingNewRiver
-                //Country countryFd = db.Countries.FirstOrDefault(x => x.CountryCode == "BG");
-                //River newRiver = new River("Lom", 92500, 1240, 739, "Black Sea");
-
-                //var newRiverCountry = new CountriesRiver()
-                //{
-                //    River = newRiver
-                //};
-
-                //countryFd.CountriesRivers.Add(newRiverCountry);
-
-                ////countryFd.Rivers
-                ////    .Add(newRiver);
-                //db.SaveChanges();
+                // Country countryFd = db.Countries.FirstOrDefault(x => x.CountryCode == "BG");
+                // River newRiver = new River("Lom", 92500, 1240, 739, "Black Sea");
+                //  RegisterNewRiver(db, countryFd, newRiver);
                 #endregion
 
-                Country countryFound = null;
-                while (countryFound == null)
-                {
-                    Console.WriteLine("Choose country to display:");
-                    Console.WriteLine("[CountryName] or [CountryId]"); //BG Bulgaria BULgaria
-                    string countryID = Console.ReadLine().ToLower();//bulgariaN134324234 //BG 
+                //  PrintDataAboutCountry(db);
+                //  DeleteAllDuplicatingRivers(db,"BG");
 
-                    countryFound = db.Countries
-                        .Include(x => x.CountriesRivers)
-                        .ThenInclude(x => x.River)
+                //  PrintOrderedRiversDataAboutCountry(db, "RU");
 
-                        .Include(x => x.MountainsCountries)
-                        .ThenInclude(x => x.Mountain)
-                        .ThenInclude(x => x.Peaks)
 
-                        .Include(x => x.MountainsCountries)
-                        .ThenInclude(x => x.Mountain)
-                        .ThenInclude(x => x.MountainsCountries)
-                        .ThenInclude(x => x.CountryCodeNavigation)
+                //Напишете метод които да извади списък с планините имащи най-високи върхове
+                //Mountain[]
+                PrintHighestMountains(db, 10);
 
-                        .FirstOrDefault(
-                        x => countryID.StartsWith(x.CountryName.ToLower())
-                        || countryID.StartsWith(x.CountryCode.ToLower()));
+                var mountains = db.Mountains
+                    .Where(m => m.Peaks.Any())
 
-                }
+                    .Where(m => !m.Peaks.Any(p => p.PeakName.StartsWith("E")))
+                    .OrderByDescending(m => m.Peaks.Max(p => p.Elevation))
+                    .Select(m => new MiniMountainDTO()
+                    {
+                        MountainName = m.MountainRange == "Karakorum" ? "KARAKORUM" : m.MountainRange,
+                        PeakHeight = m.Peaks.OrderByDescending(p => p.Elevation).First().Elevation,
+                        PeakName = m.Peaks.OrderByDescending(p => p.Elevation).First().PeakName,
+                    })
+                    .Take(5)
+                    .ToArray();
 
-                PrintData(countryFound);
 
-                //var riverNames = db.Rivers
-                //    .Where(r => r.CountriesRivers.Any(c => c.CountryCode == "BG"))
-                //    .Select(r => r.RiverName).Distinct()
-                //    .ToArray();
 
-                //List<River> approvedRivers = new List<River>();
-                //foreach (var rName in riverNames)
-                //{
-                //    approvedRivers
-                //        .Add(db.Rivers.FirstOrDefault(r => r.RiverName == rName));
-                //}
 
-                //var countryFound = db.Countries
-                //    .Include(x => x.CountriesRivers)
-                //    .ThenInclude(x => x.River)
-                //    .First(x => x.CountryName == "Bulgaria");
 
-                //countryFound.CountriesRivers = countryFound.CountriesRivers
-                //    .Where(x => approvedRivers.Contains(x.River)).ToList();
-
-                //db.SaveChanges();
-
-                //var countryFound = db.Countries
-                //    .Include(x => x.CountriesRivers)
-                //    .ThenInclude(x => x.River)
-                //    .First(x => x.CountryName == "China");
-
-                //var riverNames2 = countryFound.CountriesRivers
-                //    .OrderBy(r => r.River.Length)
-                //    .Select(x => $"{x.River.RiverName}: {x.River.Length}km")
-                //    .ToArray();
-
-                // Console.WriteLine(String.Join("\n", riverNames));
-                //Console.WriteLine(new String('=', 20));
-                //Console.WriteLine(String.Join("\n", riverNames2));
             }
+        }
+
+        private static void PrintHighestMountains(GeographyDBContext db, int count = 5)
+        {
+            var highestMountains = db.Mountains
+                .OrderByDescending(m => m.Peaks.Max(p => p.Elevation))
+                .Include(m => m.Peaks)
+                .Where(m => m.Peaks.Count() > 1)
+                .Take(count)
+                .ToArray();
+
+            foreach (var m in highestMountains)
+            {
+                Peak highestPeak = m.Peaks.OrderByDescending(p => p.Elevation).First();
+                Console.WriteLine("Mountain Name:" + m.MountainRange);
+                Console.WriteLine($"Highest Peak: {highestPeak.PeakName}. Elevation {highestPeak.Elevation} m");
+            }
+        }
+
+        private static void PrintOrderedRiversDataAboutCountry(GeographyDBContext db, string countryCode)
+        {
+            var countryFd = db.Countries
+                .Include(x => x.CountriesRivers)
+                .ThenInclude(x => x.River)
+                .First(x => x.CountryCode == countryCode);
+
+            var orderedRiversOfCountryFd = countryFd.CountriesRivers
+                .OrderBy(r => r.River.Length)
+                .Select(x => $"{x.River.RiverName}: {x.River.Length}km")
+                .ToArray();
+
+
+            Console.WriteLine(new String('=', 20));
+            Console.WriteLine(String.Join("\n", orderedRiversOfCountryFd));
+        }
+
+        private static void DeleteAllDuplicatingRivers(GeographyDBContext db, string countryCode)
+        {
+            string[] riverNames = db.Rivers
+                .Where(r => r.CountriesRivers.Any(c => c.CountryCode == countryCode))
+                .Select(r => r.RiverName).Distinct()
+                .ToArray();
+
+            List<River> approvedRivers = new List<River>();
+            foreach (var rName in riverNames)
+            {
+                approvedRivers
+                    .Add(db.Rivers.FirstOrDefault(r => r.RiverName == rName));
+            }
+
+            var countryFound = db.Countries
+                .Include(x => x.CountriesRivers)
+                .ThenInclude(x => x.River)
+                .First(x => x.CountryCode == countryCode);
+
+            countryFound.CountriesRivers = countryFound.CountriesRivers
+                .Where(x => approvedRivers.Contains(x.River)).ToList();
+
+            db.SaveChanges();
+        }
+
+        private static void PrintDataAboutCountry(GeographyDBContext db)
+        {
+            Country countryFound = null;
+            while (countryFound == null)
+            {
+                Console.WriteLine("Choose country to display:");
+                Console.WriteLine("[CountryName] or [CountryId]"); //BG Bulgaria BULgaria
+                string countryID = Console.ReadLine().ToLower();//bulgariaN134324234 //BG 
+
+                countryFound = db.Countries
+                    .Include(x => x.CountriesRivers)
+                    .ThenInclude(x => x.River)
+
+                    .Include(x => x.MountainsCountries)
+                    .ThenInclude(x => x.Mountain)
+                    .ThenInclude(x => x.Peaks)
+
+                    .Include(x => x.MountainsCountries)
+                    .ThenInclude(x => x.Mountain)
+                    .ThenInclude(x => x.MountainsCountries)
+                    .ThenInclude(x => x.CountryCodeNavigation)
+
+                    .FirstOrDefault(
+                    x => countryID.StartsWith(x.CountryName.ToLower())
+                    || countryID.StartsWith(x.CountryCode.ToLower()));
+
+            }
+
+            PrintData(countryFound);
+        }
+
+        private static void RegisterNewRiver(GeographyDBContext db, Country countryFd, River newRiver)
+        {
+            var newRiverCountry = new CountriesRiver()
+            {
+                River = newRiver
+            };
+
+            countryFd.CountriesRivers.Add(newRiverCountry);
+            //За .NET6
+            //countryFd.Rivers
+            //    .Add(newRiver);
+            db.SaveChanges();
         }
 
         private static void PrintData(Country countryFound)
@@ -117,5 +176,23 @@ namespace Geo
                 }
             }
         }
+    }
+
+
+    public class MiniMountainDTO
+    {
+        public MiniMountainDTO(int peakHeight, string peakName, string mountainName)
+        {
+            PeakHeight = peakHeight;
+            PeakName = peakName;
+            MountainName = mountainName;
+        }
+        public MiniMountainDTO()
+        {
+
+        }
+        public int PeakHeight { get; set; }
+        public string PeakName { get; set; }
+        public string MountainName { get; set; }
     }
 }
