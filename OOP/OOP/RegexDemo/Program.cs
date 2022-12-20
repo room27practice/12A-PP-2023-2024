a6bd17c39e20d18dab02c29ad3f6bece23a30c35
a <b>
2022-12-20 11:22:29 +0200

    Added Regex Task 2 solution

--- OOP/OOP/RegexDemo/Program.cs
+++ OOP/OOP/RegexDemo/Program.cs
@@ -7,32 +7,58 @@ namespace RegexDemo
     {
         static void Main(string[] args)
         {
-            Task01();
+            // Task01();
             Task02();
         }
 
         private static void Task02()
         {
-            throw new NotImplementedException();
+            var furnitures = new List<Furniture>();
+            string pattern = @"^>>([a-zA-Z\s]+)<<([1-9]\d*\.?\d+)!([1-9]\d*)$";
+            string input = Console.ReadLine();
+            while (input != "Purchase")
+            {
+                Match fur = Regex.Match(input, pattern);
+                if (fur.Success)
+                {
+                    var currentFurniture = new Furniture()
+                    {
+                        Name = fur.Groups[1].Value,
+                        Price = decimal.Parse(fur.Groups[2].Value),
+                        Quantity = int.Parse(fur.Groups[3].Value),
+                    };
+
+                    furnitures.Add(currentFurniture);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine("Bought furniture:");
+            foreach (Furniture item in furnitures)
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.WriteLine($"Total money spend: {furnitures.Sum(x => x.TotalValue):F2}");
         }
 
         public static void Task01()
         {
             string emailPattern = @"(?<user>(?<=\s)[a-z]+([\.\-_]?[a-z0-9]+))\@(?<host>[a-z]+([\-\.][a-z]+)+)";
             string userNamePattern = @"((?<=\s)[a-z]+([\.\-_]?[a-z0-9]+))(?=\@([a-z]+([\-\.][a-z]+)+))";
-            
+
             string input = Console.ReadLine();
 
             string sanitizedText = Regex.Replace(input, userNamePattern, "****");
-           
+
             Console.WriteLine(new string('=', 20));
             Console.WriteLine("Cleaned Usernames:");
-            Console.WriteLine(sanitizedText);           
+            Console.WriteLine(sanitizedText);
             Console.WriteLine(new string('=', 20));
-           
+
             MatchCollection matches = Regex.Matches(input, emailPattern);
             Match onlyFirst = Regex.Match(input, emailPattern);
-  
+
 
             Email[] emails = matches
                 .Select(m => new Email(m.Groups["user"].ToString(), m.Groups["host"].ToString()))
@@ -43,11 +69,11 @@ namespace RegexDemo
                 Console.WriteLine(match.Value);
                 Console.WriteLine("Username: " + match.Groups["user"]);
                 Console.WriteLine("Hostname: " + match.Groups["host"]);
-                Console.WriteLine(new string('=',20));
+                Console.WriteLine(new string('=', 20));
             }
 
             //string[] results = matches.Select(m => m.Value).ToArray();
-          
+
             //foreach (string item in results)
             //{
             //    Console.WriteLine(item);
@@ -69,7 +95,16 @@ namespace RegexDemo
         public string HostName { get; set; }
         public override string ToString()
         {
-            return this.UserName+"@"+this.HostName;
+            return this.UserName + "@" + this.HostName;
         }
+    }
+
+
+    public class Furniture
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public decimal TotalValue => Price * Quantity;
     }
 }
